4922c03777cbfa0b573733c9b9c54054e0662baf
Crissdavi <158458944+Crissdavi@users.noreply.github.com>
2024-08-19 18:59:54 -0500

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -7,7 +7,7 @@ let img = await (await fetch(`https://tinyurl.com/2c5hk765`)).buffer()
     contextInfo: {
     	isForwarded: true,
     forwardedNewsletterMessageInfo: {
-      newsletterJid: "120363191779210764@newsletter",
+      newsletterJid: "120363290084838811@newsletter",
       serverMessageId: 100,
       newsletterName: namechannel,
     },
