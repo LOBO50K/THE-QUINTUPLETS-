4fb3afae95e53b8a126927cffc35d9ed833b6972
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-09-07 11:03:38 -0500

    Actualizar main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -2,7 +2,7 @@ import moment from 'moment-timezone'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
-let res = await fetch('https://api.github.com/repos/StarlightsTeam/Ai-Hoshino')
+let res = await fetch('https://github.com/Crissdavi/Kuroda_haru.git')
 let json = await res.json()
 try {
 let txt = `*乂  S C R I P T  -  M A I N*\n\n`
