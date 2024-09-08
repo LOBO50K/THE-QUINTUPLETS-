445a1dba48ce95f8c6ec4850af3d3b558f2ee0f9
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-09-07 19:42:02 -0500

    Update tools-ia.js

--- plugins/tools-ia.js
+++ plugins/tools-ia.js
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch';
 
 const handler = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, 'Escribe un texto para hablar con chatgpt', m);
+if (!text) return conn.reply(m.chat, 'Escribe un texto para hablar con koruda', m);
 
 try {
-let msg = await conn.sendMessage(m.chat, {text: '*chatgpt esta escribiendo.....*'});
+let msg = await conn.sendMessage(m.chat, {text: '*koruda esta escribiendo.....*'});
 
 let userid = conn.getName(m.sender) || 'default';
 let apiurl = `https://api.guruapi.tech/ai/gpt4?username=${userid}&query=hii${encodeURIComponent(text)}`;
