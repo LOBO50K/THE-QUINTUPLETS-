da1f62d3a0b6d1d58ba5ed6f039fee30a73ae1e0
Koruda <158458944+Crissdavi@users.noreply.github.com>
2024-09-07 19:36:26 -0500

    Add files via upload

---
+++ plugins/tools-ia.js
@@ -0,0 +1,19 @@
+import fetch from 'node-fetch';
+
+const handler = async (m, { conn, text }) => {
+if (!text) return conn.reply(m.chat, 'Escribe un texto para hablar con chatgpt', m);
+
+try {
+let msg = await conn.sendMessage(m.chat, {text: '*chatgpt esta escribiendo.....*'});
+
+let userid = conn.getName(m.sender) || 'default';
+let apiurl = `https://api.guruapi.tech/ai/gpt4?username=${userid}&query=hii${encodeURIComponent(text)}`;
+let result = await fetch(apiurl);
+let response = await result.json();
+
+await conn.relayMessage(m.chat, { protocolMessage: { key: msg.key, type: 14, editedMessage: { conversation: response.msg }}}, {});
+} catch {}}
+
+handler.command = handler.help = ["chatgpt"];
+
+export default handler
