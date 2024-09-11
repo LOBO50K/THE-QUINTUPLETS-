8675c5e5a35b25d3dd3c3c9540f68aec20a20e11
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-09-11 01:43:42 -0300

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -2,7 +2,7 @@ import moment from 'moment-timezone'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
-let res = await fetch('https://github.com/LOBO50K/THE-QUINTUPLETS-.git')
+let res = await fetch('https://github.com/LOBO50K/THE-QUINTUPLETS-')
 let json = await res.json()
 try {
 let txt = `*乂  S C R I P T  -  M A I N*\n\n`
