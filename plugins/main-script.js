ca314be23f11ada24d9e11816f55971575a130ef
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-09-13 02:07:49 -0300

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -5,15 +5,15 @@ let handler = async (m, { conn, args }) => {
 let res = await fetch('https://api.github.com/repos/LOBO50K/THE-QUINTUPLETS-')
 let json = await res.json()
 try {
-let txt = '`- 乂  S C R I P T  -  M A I N`\n\n'
-    txt += `	•   *Nombre* : ${json.name}\n`
-    txt += `	•   *Visitas* : ${json.watchers_count}\n`
-    txt += `	•   *Peso* : ${(json.size / 1024).toFixed(2)} MB\n`
-    txt += `	•   *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`
-    txt += `	•   *Url* : ${json.html_url}\n`
-    txt += `	•   *Forks* : ${json.forks_count}\n`
-    txt += `	•   *Stars* : ${json.stargazers_count}\n\n`
-    txt += `> 🚩 *${textbot}*`
+let txt = '`- 乂  S C R I P T`\n\n'
+    txt += `	•   *𝕹𝖔𝖒𝖇𝖗𝖊* : ${json.name}\n`
+    txt += `	•   *𝖁𝖎𝖘𝖎𝖙𝖆𝖘* : ${json.watchers_count}\n`
+    txt += `	•   *𝕻𝖊𝖘𝖔* : ${(json.size / 1024).toFixed(2)} MB\n`
+    txt += `	•   *𝕬𝖈𝖙𝖚𝖆𝖑𝖎𝖟𝖆𝖉𝖔* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`
+    txt += `	•   *𝖚𝖗𝖑* : ${json.html_url}\n`
+    txt += `	•   *𝕱𝖔𝖗𝖐𝖘* : ${json.forks_count}\n`
+    txt += `	•   *𝕾𝖙𝖆𝖗𝖘* : ${json.stargazers_count}\n\n`
+    txt += `> 💌 *${textbot}*`
 let img = await (await fetch(`https://tinyurl.com/247ebo9v`)).buffer()
 
 await conn.sendAi(m.chat, botname, textbot, txt, img, img, canal, m)
@@ -22,6 +22,6 @@ await m.react('✖️')
 }}
 handler.help = ['script']
 handler.tags = ['main']
-handler.command = ['script', 'sc']
+handler.command = ['script', 'sc', 'repo', 'repository']
 handler.register = true 
 export default handler
