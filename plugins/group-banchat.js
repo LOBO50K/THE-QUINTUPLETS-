78cb2525ac908c11bf3f7b2941a0984bacd197e1
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-09-13 16:08:12 -0300

    Update group-banchat.js

--- plugins/group-banchat.js
+++ plugins/group-banchat.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, isAdmin, isROwner }) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    await conn.reply(m.chat, `🚩 Chat Baneado con exito.`, m, rcanal)
+    await conn.reply(m.chat, ` ˚₊· ͟͟͞͞➳❥ 🌄⚘ 🍪  🍰  𝐻𝒜𝒮𝒯𝒜 𝐿𝒰𝐸𝒢❁  🎀  🍪 🌄⚘.`, m, rcanal)
     await m.react('✅')
 }
 handler.help = ['banearbot']
