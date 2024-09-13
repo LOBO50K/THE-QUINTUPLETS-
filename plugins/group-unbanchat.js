5ace72bd22940c3a4ea432a17a7ce78547f526b7
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-09-13 16:13:08 -0300

    Update group-unbanchat.js

--- plugins/group-unbanchat.js
+++ plugins/group-unbanchat.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, isAdmin, isROwner} ) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = false
-    await conn.reply(m.chat, '🚩 Bot activo en este grupo.', m, rcanal)
+    await conn.reply(m.chat, ' ִ✧༝┉˚*❋❤❋*˚┉༝✧ ࣪𖤐◞ ꙳ ๋࣭ ⭑ `✨𝕳𝕺𝕷𝕬 𝕯𝕰 𝕹𝖀𝕰𝖁𝕺✨ੈ✩‧₊˚✧༝┉˚*❋❤❋*˚┉༝✧ .', m, rcanal)
     await m.react('✅')
 }
 handler.help = ['desbanearbot']
