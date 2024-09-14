a41c5a594cf25beeb02e06358d0005af8e8b1060
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-09-14 12:50:11 -0300

    Update nsfw-rule34.js

--- plugins/nsfw-rule34.js
+++ plugins/nsfw-rule34.js
@@ -1,8 +1,8 @@
 import Starlights from "@StarlightsTeam/Scraper"
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!global.db.data.chats[m.chat].nsfw) return conn.reply(m.chat, `🚩 El grupo no admite contenido *Nsfw.*\n\n> Para activarlo un *Administrador* debe usar el comando */nsfw on*`, m, rcanal)
-if (!text) return m.reply('🚩 Ingresa el nombre de la imágen que estas buscando.')
+if (!global.db.data.chats[m.chat].nsfw) return conn.reply(m.chat, `❍⌇─➭ 𝑵𝒐 𝒆𝒔𝒕𝒂 𝒑𝒆𝒓𝒎𝒊𝒕𝒊𝒅𝒐 𝒆𝒍 𝑵𝑺𝑭𝑾 𝒆𝒏 𝒆𝒔𝒕𝒆 𝒈𝒓𝒖𝒑𝒐﹀﹀︵↷.*\n\n> ∴.·:*¨¨*:·. ☙.·:*¨ ¨*:·.♡ .·:*¨ ¨*:·. ❧.·:*¨ ¨*:·.∴𝑺𝒐𝒍𝒐 𝒖𝒏 𝒂𝒅𝒎𝒊𝒏 𝒍𝒐 𝒑𝒖𝒆𝒅𝒆 𝒂𝒄𝒕𝒊𝒗𝒂𝒓 𝒖𝒔𝒂𝒏𝒅𝒐 𝒆𝒍 /𝑶𝑵 𝑵𝑺𝑭𝑾∴.·:*¨¨*:·. ☙.·:*¨ ¨*:·.♡ .·:*¨ ¨*:·. ❧.·:*¨ ¨*:·.∴`, m, rcanal)
+if (!text) return m.reply(' .˚₊‧༉︶︶︶︶︶︶︶︶︶༉‧₊˚.𝑯𝒐𝒍𝒂 𝒑𝒆𝒓𝒗𝒆𝒓𝒕𝒊𝒅𝒐,𝒒𝒖𝒆 𝒊𝒎𝒂𝒈𝒆𝒏 𝒃𝒖𝒔𝒄𝒂𝒔?.˚₊‧༉︶︶︶︶︶︶︶︶︶༉‧₊˚..')
 await m.react('🕓')
 try {
 let { dl_url } = await Starlights.rule34(text)
