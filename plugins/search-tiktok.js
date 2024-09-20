3e107bbec84e91ac1cf92bba93155de2a0f8e9e2
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-09-20 00:45:06 -0300

    Update search-tiktok.js

--- plugins/search-tiktok.js
+++ plugins/search-tiktok.js
@@ -1,37 +1,21 @@
-import axios from 'axios'
-import Starlights from '@StarlightsTeam/Scraper'
-
-let handler = async (m, { conn, usedPrefix, command, text, args }) => {
-  if (!text) return conn.reply(m.chat, `🚩 Ingresa el nombre video que deseas buscar en TikTok.\n\nEjemplo:\n> *${usedPrefix + command}* Ai Hoshino Edit`, m, rcanal)
-  
-  await m.react('🕓')
-  let img = await (await axios.get('https://i.ibb.co/89j7Fcy/file.jpg', { responseType: 'arraybuffer' })).data
-
-  try {
-    let data = await Starlights.tiktokSearch(text)
-
-    if (data && data.length > 0) {
-      let txt = `*乂  T I K T O K  -  S E A R C H*`
-      for (let i = 0; i < (50 <= data.length ? 50 : data.length); i++) {
-        let video = data[i]
-        txt += `\n\n`
-        txt += `  *» Nro* : ${i + 1}\n`
-        txt += `  *» Título* : ${video.title}\n`
-        txt += `  *» Autor* : ${video.author}\n`
-        txt += `  *» Url* : ${video.url}`
-      }
-      await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
-      await m.react('✅')
-    } else {
-      await conn.react('✖️')
-    }
-  } catch {
-    await m.react('✖️')
-  }
+import axios from 'axios';
+let handler = async (m, { conn, text }) => {
+if (!text) return conn.reply(m.chat, '✧ Ingresa un texto para realizar la búsqueda.', m);
+const getRandomElement = (array) => array[Math.floor(Math.random() * array.length)];
+try {
+    const { data } = await axios.get(`https://apis-starlights-team-cbb6f3a3.koyeb.app/starlight/tiktoksearch?text=${text}`);
+    const results = data.data;
+    if (!results.length) return conn.reply(m.chat, '✧ No se encontraron resultados.', m);
+    const randomResult = getRandomElement(results);
+    const videoUrl = randomResult.nowm;
+    await conn.sendFile(m.chat, videoUrl, 'tts.mp4', `❀ *TIKTOK SEARCH*\n✰ *Resultados para:*\n> ${text}`, m);
+} catch (error) {
+    console.log(error);
 }
-handler.tags = ['search']
-handler.help = ['tiktoksearch *<búsqueda>*']
-handler.command = ['tiktoksearch', 'tiktoks']
-handler.register = true
+};
 
-export default handler
+handler.command = ['tts', 'tiktokvid', 'ttvid'];
+handler.help = ['tiktoksearch <texto>'];
+handler.registrado = true;
+handler.diamantes = 1;
+export default handler;
