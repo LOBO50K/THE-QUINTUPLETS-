3c35db654f6629aaa6a79b3e7731a582b0ddcf43
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-09-26 09:33:58 -0300

    Update Nakano-sad.js

--- plugins/Nakano-sad.js
+++ plugins/Nakano-sad.js
@@ -30,7 +30,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 }
 
 handler.help = ['triste @tag'];
-handler.tags = ['fun'];
+handler.tags = ['nakano react'];
 handler.command = ['sad','triste'];
 handler.group = true;
 
