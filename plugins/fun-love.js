6f332baa1aecf589a298ed3023bf5ffeb214ebd1
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-09-30 00:29:57 -0300

    Update fun-love.js

--- plugins/fun-love.js
+++ plugins/fun-love.js
@@ -1,4 +1,5 @@
-let handler = async (m, { conn, command, text }) => {
+
+/*let handler = async (m, { conn, command, text }) => {
 let love = `*❤️❤️ MEDIDOR DE AMOR ❤️❤️*
 *El amor de ${text} por ti es de* *${Math.floor(Math.random() * 100)}%* *de un 100%*
 *Deberias pedirle que sea tu  novia/o ?*
@@ -7,4 +8,4 @@ m.reply(love, null, { mentions: conn.parseMention(love) })}
 handler.help = ['love *@user*']
 handler.tags = ['fun']
 handler.command = /^(love)$/i
-export default handler
+export default handler//*
