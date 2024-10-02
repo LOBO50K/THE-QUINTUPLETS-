a4b9ce240b6ee7716080d2dea12ee6598f045a11
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-10-02 01:40:33 -0300

    Update rpg-stars.js

--- plugins/rpg-stars.js
+++ plugins/rpg-stars.js
@@ -6,19 +6,19 @@ let handler = async (m, {conn, usedPrefix}) => {
     let user = global.db.data.users[who]
     let name = conn.getName(who)
     if (!(who in global.db.data.users)) return conn.reply(m.chat, '🚩 El usuario no se encuentra en mi base de Datos.', m, rcanal).then(_ => m.react('✖️'))
-    let img = await (await fetch(`https://tinyurl.com/27t9qss5`)).buffer()
-    let txt = ` –  *S T A R S  -  U S E R*\n\n`
-        txt += `┌  ✩  *🪪 Nombre* : ${user.name}\n`
-        txt += `│  ✩  *💴 Zenis* : ${toNum(user.limit)} ( *${user.limit}* )\n`
-        txt += `│  ✩  *🏦 Bank* : ${toNum(user.bank)} ( *${user.bank}* )\n`
-        txt += `└  ✩  *💫 XP* : ${toNum(user.exp)} ( *${user.exp}* )`
+    let img = await (await fetch(`https://f.uguu.se/LJGAUJcD.jpg`)).buffer()
+    let txt = ` –  *B A L -  U S E R*\n\n`
+        txt += `┌  ✩  *♧ Nombre * : ${user.name}\n`
+        txt += `│  ✩  *¥ Yenes* : ${toNum(user.limit)} ( *${user.limit}* )\n`
+        txt += `│  ✩  *◇ Banco* : ${toNum(user.bank)} ( *${user.bank}* )\n`
+        txt += `└  ✩  *☆ exp* : ${toNum(user.exp)} ( *${user.exp}* )`
     let mentionedJid = [who]
         
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
 }
-handler.help = ['Zenis']
+handler.help = ['Yenes']
 handler.tags = ['rpg']
-handler.command = ['coins', 'wallet', 'cartera', 'Zenis', 'stars', 'bal', 'balance']
+handler.command = ['coins', 'wallet', 'cartera', 'Yenes', 'yenes', 'bal', 'balance']
 handler.register = true 
 export default handler
 
