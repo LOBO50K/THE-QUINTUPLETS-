9e2a4f2b9e8c66606bf724b1874c1cac13664d60
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-10-03 01:50:22 -0300

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,16 +1,16 @@
 import Starlights from "@StarlightsTeam/Scraper"
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat, '🚩 Ingrese el enlace de un archivo de Mediafire.', m, rcanal)
-if (!args[0].match(/mediafire/gi)) return conn.reply(m.chat, 'El enlace deve ser de un archivo de Mediafire.', m, rcanal)
+if (!args[0]) return conn.reply(m.chat, '🚩 Ingrese el enlace de un archivo de Mediafire.', m,)
+if (!args[0].match(/mediafire/gi)) return conn.reply(m.chat, 'El enlace deve ser de un archivo de Mediafire.', m,)
 await m.react('🕓')
 try {
 let { title, ext, aploud, size, dl_url } = await Starlights.mediafire(args[0])
-let txt = `乂  *M E D I A F I R E  -  D O W N L O A D*\n\n`
-    txt += `	✩  *Nombre* : ${title}\n`
-    txt += `	✩  *Peso* : ${size}\n`
-    txt += `	✩  *Publicado* : ${aploud}\n`
-    txt += `	✩  *MimeType* : ${ext}\n\n`
+let txt = ` *DESCARGAS NAKANO*\n\n`
+    txt += `	᪥ *Nombre* : ${title}\n`
+    txt += `	᪥  *Peso* : ${size}\n`
+    txt += `	᪥  *Publicado* : ${aploud}\n`
+    txt += `    ᪥ *MimeType* : ${ext}\n\n`
     txt += `*- ↻ El archivo se esta enviando espera un momento, soy lenta. . .*`
 let img = await (await fetch('https://i.ibb.co/wLQFn7q/logo-mediafire.jpg')).buffer()
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
@@ -22,6 +22,6 @@ await m.react('✖️')
 handler.help = ['mediafire'].map(v => v + ' *<url>*')
 handler.tags = ['downloader', 'premium']
 handler.command = ['mediafire', 'mdfire', 'mf']
-handler.premium = true 
+handler.premium = false
 
 export default handler
