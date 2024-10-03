fc80ca1d77c8c65d567f5669388aa8554a0622c4
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-10-03 01:18:55 -0300

    Update rpg-lb.js

--- plugins/rpg-lb.js
+++ plugins/rpg-lb.js
@@ -12,7 +12,7 @@ let text = `
 〔❨᪥❩〕Los usuarios con más *Yenes* son:
 
 ᪥ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-᪥ ${sortedLim.slice(0, len).map(({ jid, limit }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]}           *Total* : *${limit} ¥*`).join`\n᪥ ⇢ `}
+᪥ ${sortedLim.slice(0, len).map(({ jid, limit }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]}      *Total* : *¥${limit} Yenes*`).join`\n᪥ ⇢ `}
 
 `
   m.reply(text, null, { mentions: conn.parseMention(text) })
