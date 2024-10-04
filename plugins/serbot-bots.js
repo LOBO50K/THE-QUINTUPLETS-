c41f32d5fce0d8958d12cb30d146676210355598
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-10-04 04:02:12 -0300

    Update serbot-bots.js

--- plugins/serbot-bots.js
+++ plugins/serbot-bots.js
@@ -8,16 +8,16 @@ global.conns.forEach((conn) => {
 if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
 uniqueUsers.set(conn.user.jid.replace(/[^0-9]/g, ''), conn.user)}})
 
-const message = Array.from(uniqueUsers.values()).map((user, index) => `  *${index + 1}* : @${user.jid.replace(/[^0-9]/g, '')}\n│  ✩  *Link* : http://wa.me/${user.jid.replace(/[^0-9]/g, '')}\n  *Nombre* : ${user.name || '▰▱▰▱▰▱▰▱▰▱▰▱▰▱𝑵𝒂𝒌𝒂𝒏𝒐▰▱▰▱▰▱▰▱▰▱▰▱▰▱'}\n`
+const message = Array.from(uniqueUsers.values()).map((user, index) => `┌  ✩  *${index + 1}* : @${user.jid.replace(/[^0-9]/g, '')}\n  *Link* : http://wa.me/${user.jid.replace(/[^0-9]/g, '')}\n  *Nombre* : ${user.name || '▰▱▰▱▰▱▰▱▰▱▰▱▰▱𝑵𝒂𝒌𝒂𝒏𝒐▰▱▰▱▰▱▰▱▰▱▰▱▰▱'}\n`
   ).join('\n')
   
 const replyMessage = message.length === 0 ? "" : message
 const totalUsers = uniqueUsers.size;
-const responseMessage = `${` –   ▰▱▰▱▰▱▰▱▰▱▰▱▰▱ ❀ ✿ ღ𝑩𝒐𝒕𝒔ღ ❀ ✿\n${replyMessage.trim()}`.trim()}`
+const responseMessage = `${` –   ▰▱▰▱▰▱▰▱▰▱▰▱▰▱ ❀ ✿ ღ𝑩𝒐𝒕𝒔ღ ❀ ✿\n\n${replyMessage.trim()}`.trim()}`
   
+let img = await (await fetch(`https://i.ibb.co/cvmV7CV/file.jpg`)).buffer()
 
-  
-  await _envio.sendFile(m.chat, img, 'thumbnail.jpg', responseMessage, m, false, { mentions: _envio.parseMention(responseMessage) })
+await _envio.sendFile(m.chat, img, 'thumbnail.jpg', responseMessage, m, false, { mentions: _envio.parseMention(responseMessage) })
 }
 handler.command = ['listjadibot', 'bots']
 handler.help = ['bots']
