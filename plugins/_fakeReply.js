5279ca1d8620f47647fe8f6aedaf6b6ce99dc91f
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-10-04 02:13:50 -0300

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -17,7 +17,7 @@ let img = await (await fetch(`https://tinyurl.com/24tl693s`)).buffer()
 
  global.adReply = {
 	    contextInfo: { 
-             forwardingScore:, 
+             forwardingScore:9999, 
                  isForwarded: false, 
                     externalAdReply: {
 				    showAdAttribution: true,
