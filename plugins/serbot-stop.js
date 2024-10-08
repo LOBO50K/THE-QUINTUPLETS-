f6a87d1f9a3130b6468b9eac25da1401d809c012
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-10-08 20:58:20 -0300

    Update serbot-stop.js

--- plugins/serbot-stop.js
+++ plugins/serbot-stop.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
   if (global.conn.user.jid === conn.user.jid) {
   } else {
-    await conn.reply(m.chat, `Adiós Ai :(`, m, rcanal)
+    await conn.reply(m.chat, `NAKANO SE DESPIDE :D`, m, rcanal)
     conn.ws.close()
   }
 }
