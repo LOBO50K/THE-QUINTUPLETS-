ff0ff4ce7e955aa031822ea2f6d6d628dc265b01
𝕷𝖔𝖇𝖔 <playperfect344@gmail.com>
2024-10-11 04:13:05 -0300

    Update rpg-deposit.js

--- plugins/rpg-deposit.js
+++ plugins/rpg-deposit.js
@@ -2,22 +2,22 @@ import db from '../lib/database.js'
 
 let handler = async (m, { args }) => {
    let user = global.db.data.users[m.sender]
-   if (!args[0]) return m.reply('🚩 Ingresa la cantidad de *⭐ Estrellas* que deseas Depositar.')
-   if ((args[0]) < 1) return m.reply('🚩 Ingresa una cantidad válida de *⭐ Estrellas.')
+   if (!args[0]) return m.reply('᪥ Ingresa la cantidad de *¥ Yenes* que deseas Depositar.')
+   if ((args[0]) < 1) return m.reply('᪥ Ingresa una cantidad válida de *¥ Yenes.')
    if (args[0] == 'all') {
       let count = parseInt(user.limit)
       user.limit -= count * 1
       user.bank += count * 1
-      await m.reply(`Depositaste *${count} ⭐ Estrellas* al Banco.`)
+      await m.reply(`Depositaste *¥${count} Yenes* al Banco.`)
       return !0
    }
-   if (!Number(args[0])) return m.reply('🚩 La cantidad deve ser un Numero.')
+   if (!Number(args[0])) return m.reply('᪥ La cantidad deve ser un Numero.')
    let count = parseInt(args[0])
-   if (!user.limit) return m.reply('No tienes *⭐ Estrellas* en la Cartera.')
-   if (user.limit < count) return m.reply(`Solo tienes *${user.limit} ⭐ Estrellas* en la Cartera.`)
+   if (!user.limit) return m.reply('᪥ No tienes *¥ Yenes* en la Cartera.')
+   if (user.limit < count) return m.reply(`᪥ Solo tienes *¥${user.limit} Yenes* en la Cartera.`)
    user.limit -= count * 1
    user.bank += count * 1
-   await m.reply(`Depositaste *${count} ⭐ Estrellas* al Banco.`)
+   await m.reply(`᪥ Depositaste *¥${count} Yenes* al Banco.`)
 }
 
 handler.help = ['depositar']
